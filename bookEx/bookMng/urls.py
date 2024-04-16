24551764eb76edd20cd319c85a3f07badf063f3c
kzhangg15 <63492687+kzhangg15@users.noreply.github.com>
2024-04-15 18:28:04 -0700

    Delete bookEx directory

--- bookEx/bookMng/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('postbook', views.postbook, name='postbook'),
-    path('displaybooks', views.displaybooks, name='displaybooks')
-]
